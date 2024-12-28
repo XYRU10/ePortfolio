f091fc564038648830d0c616869b5cb0e8329140
XYRU10 <bandiala.xyrujon11@gmail.com>
2024-12-28 21:01:06 +0800

    initalizing pls work this time

--- w/Javascript/scripts.js
+++ w/Javascript/scripts.js
@@ -1,4 +1,21 @@
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
+// Dynamically load sections
+function loadSection(id, file) {
+    fetch(file)
+      .then(res => {
+        if (!res.ok) throw new Error(`Failed to load ${file}`);
+        return res.text();
+      })
+      .then(content => {
+        document.getElementById(id).innerHTML = content;
+      })
+      .catch(err => console.error(err));
+  }
+  
+  // Load each section
+  loadSection('header', 'header.html');
+  loadSection('home', 'home.html');
+  loadSection('about', 'about.html');
+  loadSection('projects', 'projects.html');
+  loadSection('contact', 'contact.html');
+  loadSection('footer', 'footer.html');
+  
